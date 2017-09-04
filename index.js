91f64cf2509faca6f510203b4cc2d1aee8f4289c
Jim Breslin <jbreslin33@gmail.com>
2017-09-04 18:11:13 -0400

    turns light off

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const app = express()
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
+	require('child_process').spawn('sh', ['off.sh'], {stdio: 'inherit'});
 })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(4000, function () {
+  console.log('Lightswitch off listening on port 4000!')
 })
 
-//require('child_process').spawn('sh', ['off.sh'], {stdio: 'inherit'});
